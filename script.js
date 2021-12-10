58c9a2f153d4f6bfd2681aa86c0bcfbce988da34
Davi Freitas <df828316@gmail.com>
2021-12-10 19:30:49 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const botaoTarefa = document.getElementById('criar-tarefa');
 const inputTarefa = document.getElementById('texto-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
+const tarefas = document.getElementsByTagName('li');
 
-function criarTarefa () {    
-    const elementLi = document.createElement('li');
-    elementLi.innerText = inputTarefa.value;
-    listaTarefa.appendChild(elementLi);
-    inputTarefa.value = '';
+function createTask() {
+  const createElementLi = document.createElement('li');
+  createElementLi.innerText = inputTarefa.value;
+  listaTarefa.appendChild(createElementLi);
+  inputTarefa.value = '';
+  percorrerTarefas()
+}
+botaoTarefa.addEventListener('click', createTask);
+
+function highLight(event) {
+  const highlight = document.querySelector('.highlight');
+  //highlight.classList.remove('highlight');
+  event.target.classList.add('highlight');
 }
 
-botaoTarefa.addEventListener('click', criarTarefa);
+function percorrerTarefas() {
+  for (let index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].addEventListener('click', highLight);
+  }
+}
