e56924f4e05117190a3d4d6205be9feff0010b5b
Davi Freitas <df828316@gmail.com>
2021-12-13 15:19:20 -0300

    requisito 10

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const botaoTarefa = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
 const tarefas = document.getElementsByTagName('li');
+const botaoApagar = document.getElementById('apaga-tudo');
 
 function createTask() {
   const createElementLi = document.createElement('li');
@@ -12,17 +13,27 @@ function createTask() {
 }
 botaoTarefa.addEventListener('click', createTask);
 
+function clearTask() {
+  input.value = '';
+  // const deletar = listaTarefa.childNodes;
+  // for (let index = 0; index < tarefas.length; index += 1) {
+    // listaTarefa.removeChild(deletar[index]);
+  // }
+  listaTarefa.innerHTML = '';
+}
+botaoApagar.addEventListener('click', clearTask);
+
 function highLight(event) {
   const highlight = document.querySelector('.highlight');
-  if (highlight !== null){
-  highlight.classList.remove('highlight');
+  if (highlight !== null) {
+    highlight.classList.remove('highlight');
   }
   event.target.classList.add('highlight');
 }
 
 function completar(event) {
   const completed = document.querySelector('.completed');
-  if (completed !== null){
+  if (completed !== null) {
     completed.classList.remove('completed');
   }
   event.target.classList.add('completed');
